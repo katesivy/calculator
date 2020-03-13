68fe93180fc2498483dd61a87413faa73db83f39
katesivy <58646769+katesivy@users.noreply.github.com>
2020-03-13 10:11:26 -0400

    grid

--- main.js
+++ main.js
@@ -2,44 +2,52 @@
 
 // set up class (parent) to store everything
 class Element {
-    constructor(elementType, name, id) {
-        this.element = document.createElement(elementType),
-            this.name = name,
-            this.id = id
+    constructor(elementType, className, id) {
+        this.element = document.createElement(elementType);
+        this.element.className = className;
+        this.element.id = id;
     };
 }
 
 function createGrid() {
-    // use child class to set up rows/cols and calculator
+    // const app = document.getElementById("app");
     var container = new Element("div", "container", "app");
-    var firstRow = new Element("div", "row", "row");
-    var firstCol = new Element("div", "col", "col-lg-4 col-sm-2");
-    firstRow.appendChild(firstCol);
-    var secondCol = new Element("div", "col", "col-lg-4 col-sm-2 border-dark");
+    var firstRow = new Element("div", "row", "row1");
+    var firstCol = new Element("div", "col-lg-4", "col1");
+    firstRow.element.appendChild(firstCol.element);
 
+    var secondCol = new Element("div", "col-lg-4 pt-5 border border-dark", "col2");
+    var displayRow = new Element("div", "row", "displayRow");
+    var displayCol = new Element("div", "col-4", "displayCol");
     // set up grid in secondCol
-    var count = 0;
 
-    for (var i = 0; i < 6; i++) {
-        var row = document.createElement("div");
-        row.setAttribute('id', 'row' + i);
-        row.className = "row";
+
+    var count = 0;
+    for (var i = 0; i < 5; i++) {
+        var gridRows = new Element("div", "row", "gridRows");
         for (var j = 0; j < 4; j++) {
-            var col = document.createElement("div");
-            col.setAttribute('id', count);
+            var gridCols = new Element("div", "col-1 pt-5 text-center border border-dark bg-primary", `gridCols${count}`) 
+            // col.setAttribute('id', count);
             // set up click event for squares
             // col.addEventListener('click', clickSquare);
-            col.className = "col-1  text-center px-lg-5 mx-sm-1 p-2 border border-dark bg-primary";
-            row.appendChild(col);
-            count++;
+
+            gridRows.element.appendChild(gridCols.element);
+         count++;
         }
+        displayCol.element.appendChild(gridRows.element);
 
-        secondCol.appendChild(row);
     }
-    firstRow.appendChild(secondCol);
-    container.appendChild(firstRow);
-    app.appendChild(container);
 
-    var thirdCol = new Element("div", "col", "col-lg-4 col-sm-2");
-    firstRow.appendChild(thirdCol);
+    //append display row & col 
+    secondCol.element.appendChild(displayRow.element);
+    displayRow.element.appendChild(displayCol.element);
+    // append 1st row to container
+    container.element.appendChild(firstRow.element);
+    app.appendChild(container.element);
+    firstRow.element.appendChild(secondCol.element);
+
+
+    var thirdCol = new Element("div", "col-lg-4", "col3");
+    firstRow.element.appendChild(thirdCol.element);
 }
+createGrid();
